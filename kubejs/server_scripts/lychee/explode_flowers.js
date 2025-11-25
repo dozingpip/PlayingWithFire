39062d90dda53e63c0884f6ce763bdb3ca2b410b
dozingpip <dozingpip@gmail.com>
2025-11-25 06:45:48 -0500

    explode mystical flowers into petal blocks

---
+++ kubejs/server_scripts/lychee/explode_flowers.js
@@ -0,0 +1,30 @@
+const colors = DyeColor.values();
+ServerEvents.recipes(event => {
+    for(var colorId in colors)
+    {
+        var color = DyeColor.byId(colorId)
+        event.custom({
+            type: "lychee:block_exploding",
+            block_in: {blocks:[`botania:${color}_mystical_flower`]},
+            post: [
+                {
+                    "type": "place",
+                    "block": `botania:${color}_petal_block`
+                }
+            ]
+        }
+        ).id(`playingwithfire:explosion/block/${color}_petal_block`)
+        
+        event.custom({
+            type: "lychee:item_exploding",
+            item_in: [{item:`botania:${color}_mystical_flower`}],
+            post: [
+                {
+                    "type": "place",
+                    "block": `botania:${color}_petal_block`
+                }
+            ]
+        }
+        ).id(`playingwithfire:explosion/item/${color}_petal_block`)
+    }
+});
