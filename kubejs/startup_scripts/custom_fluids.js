982d7fd48c336812f5592487f33d37c7345750a6
dozingpip <dozingpip@gmail.com>
2025-11-25 06:52:28 -0500

    custom fluid example

---
+++ kubejs/startup_scripts/custom_fluids.js
@@ -0,0 +1,4 @@
+// StartupEvents.registry('fluid', event => {
+//     event.create('awkward_potion')
+//     .displayName('Awkward Potion').stillTexture('create:fluid/potion_still').flowingTexture('create:fluid/potion_flow').color(0x0b03fc)
+// })
